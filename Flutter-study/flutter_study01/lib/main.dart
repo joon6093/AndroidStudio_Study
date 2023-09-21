1a76bcc35415dff9605df5ac4d3f0eb14b2cd9b6
Jeyong <118044367+joon6093@users.noreply.github.com>
2023-09-21 11:42:27 +0900

    [ADD] DropdownButton 위젯 기능 이용

--- Flutter-study/flutter_study01/lib/main.dart
+++ Flutter-study/flutter_study01/lib/main.dart
@@ -1,28 +1,50 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue
-      ),
-      home: const MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
     );
   }
 }
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+
+const List<String> list = <String>['Apple','Banana','Mango', 'Melon'];
+
+class MyHomePage extends StatefulWidget{
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String dropDownValue = list.first;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('제목'),
-      ),
-      body: Image.asset(
-          'assets/누워있는렉돌.png'
-      ),
+        appBar: AppBar(
+          title: Text('제목'),
+        ),
+        body: DropdownButton<String>(
+          items: list.map<DropdownMenuItem<String>>((String value){
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          value: dropDownValue,
+          icon: Icon(Icons.arrow_drop_down),
+          iconSize: 24,
+          elevation: 8,
+          onChanged:(String? value){
+            setState(() {
+              dropDownValue = value!;
+            });
+          },
+        ),
     );
   }
 }
