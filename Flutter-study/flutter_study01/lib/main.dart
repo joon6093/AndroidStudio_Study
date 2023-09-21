ef135560aeab60777f391d6966ea70e4cfe4ff7c
Jeyong <118044367+joon6093@users.noreply.github.com>
2023-09-21 10:07:41 +0900

    StatefulWidget 기능 이용

--- Flutter-study/flutter_study01/lib/main.dart
+++ Flutter-study/flutter_study01/lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
-void main() => runApp(const MyApp());
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+void main() => runApp(new MyApp());
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Stateful Widget Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.deepOrange
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget { //StateFulWidget 클래스
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> { //상태 클래스
+  var _text = 'Hello';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HelloWorld'),
+      ),
+      body: Text(
+        _text,
+        style: TextStyle(fontSize: 40),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _text = 'World';
+            });
+          }
       ),
     );
   }
