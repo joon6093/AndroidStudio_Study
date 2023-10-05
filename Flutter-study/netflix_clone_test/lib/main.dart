501b532ca67648b8cc417f7d4de33522b118032f
Jeyong <joon6093@naver.com>
2023-10-06 02:48:43 +0900

    [ADD] 넷플릭스 클론 프로젝트

---
+++ Flutter-study/netflix_clone_test/lib/main.dart
@@ -0,0 +1,48 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/screen/home_screen.dart';
+import 'package:netflix_clone_test/screen/more_screen.dart';
+import 'package:netflix_clone_test/widget/bottom_bar.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bbongflix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        hintColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              Container(
+                child: Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('save'),
+                ),
+              ),
+              MoreScreen(),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
---
+++ Flutter-study/netflix_clone_test/lib/widget/carousel_slider.dart
@@ -0,0 +1,163 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import '../model/model_movie.dart';
+import '../screen/detail_screen.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+
+  CarouselImage({required this.movies});
+
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  late List<Movie> movies;
+  late List<Widget> images;
+  late List<String> keywords;
+  late List<bool> likes;
+  int _currentPage = 0;
+  late String _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
+    keywords = movies.map((m) => m.keyword).toList();
+    likes = movies.map((m) => m.like).toList();
+    _currentKeyword = keywords[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+          ),
+          CarouselSlider.builder(
+            itemCount: images.length,
+            itemBuilder: (BuildContext context, int index, int realIndex) {
+              return images[index];
+            },
+            options: CarouselOptions(
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentPage = index;
+                  _currentKeyword = keywords[_currentPage];
+                });
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      likes[_currentPage]
+                          ? IconButton(
+                              icon: Icon(Icons.check),
+                              onPressed: () {},
+                            )
+                          : IconButton(
+                              icon: Icon(Icons.add),
+                              onPressed: () {},
+                            ),
+                      Text(
+                        '내가 찜한 콘텐츠',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3),
+                        ),
+                        Text(
+                          '재생',
+                          style: TextStyle(color: Colors.black),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.info),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                              fullscreenDialog: true,
+                              builder: (BuildContext context) {
+                                return DetailScreen(
+                                  movie: movies[_currentPage],
+                                );
+                              }));
+                        },
+                      ),
+                      Text(
+                        '정보',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: makeIndicator(movies.length, _currentPage),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+List<Widget> makeIndicator(int movieCount, int currentPage) {
+  List<Widget> results = [];
+  for (var i = 0; i < movieCount; i++) {
+    results.add(Container(
+      width: 8,
+      height: 8,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: currentPage == i
+            ? Color.fromRGBO(255, 255, 255, 0.9)
+            : Color.fromRGBO(255, 255, 255, 0.4),
+      ),
+    ));
+  }
+
+  return results;
+}
