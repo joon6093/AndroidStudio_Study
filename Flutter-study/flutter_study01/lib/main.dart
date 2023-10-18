a45ab37f15526334563f768787ce73093f988916
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-10-18 17:12:27 +0900

    [ADD] 퓨저 빌더를 통해 비동기 사용해보기

--- Flutter-study/flutter_study01/lib/main.dart
+++ Flutter-study/flutter_study01/lib/main.dart
@@ -1,63 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+  Future<int> sum() {
+    return Future<int>(() {
+      var sum = 0;
+      for (int i = 0; i < 5000000000; i++) {
+        sum += i;
+      }
+      return sum;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hero Widget Demo',
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hero Widget Demo'),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondPage()));
-        },
-        child: Hero(
-          tag: 'icon',
-          child: Icon(
-            Icons.sentiment_satisfied,
-            size: 100.0,
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Future and FutureBuilder'),
         ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hero Widget Demo'),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
+        body: FutureBuilder(
+          future: sum(),
+          builder: (context, snapshot) {
+            //두번째 매개변수에 future 데이터 가 전달
+            if (snapshot.hasData) {
+              return Center(
+                child: Text(
+                  '${snapshot.data}',
+                  style: const TextStyle(color: Colors.black, fontSize: 30),
+                ),
+              );
+            }
+            return const Center(
+              child: Text(
+                'waiting',
+                style: TextStyle(color: Colors.black, fontSize: 30),
+              ),
+            );
           },
-          child: Hero(
-            tag: 'icon',
-            child: Icon(
-              Icons.sentiment_satisfied,
-              size: 200.0,
-            ),
-          ),
         ),
       ),
     );
