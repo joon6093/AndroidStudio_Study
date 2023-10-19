0a59998134ee830e3cf08ae18d7e1063326bed9f
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-10-19 13:57:39 +0900

    [INIT] 초기 환경 구축

--- Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/MainActivity.kt
+++ Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/MainActivity.kt
@@ -1,46 +1,11 @@
 package kr.ac.kumoh.ce.s20190633.s23w07intentdata
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import kr.ac.kumoh.ce.s20190633.s23w07intentdata.ui.theme.S23W07IntentDataTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            S23W07IntentDataTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S23W07IntentDataTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
