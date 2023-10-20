5ebe1bbe7257067beaa9204f3a4c64d81947c8f6
Jeyong <joon6093@naver.com>
2023-10-21 04:04:22 +0900

    [ADD] 코드 주석 추가

--- Android-study/S23W06Intent/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w06intent/MainActivity.kt
+++ Android-study/S23W06Intent/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w06intent/MainActivity.kt
@@ -19,15 +19,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat.startActivity
 import kr.ac.kumoh.ce.s20190633.s23w06intent.ui.theme.S23W06IntentTheme
 
+// 메인 액티비티 클래스 정의
 class MainActivity : ComponentActivity() {
+    // 액티비티가 생성될 때 호출되는 함수
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // 앱의 테마를 설정하여 UI 컴포넌트들을 표시
             S23W06IntentTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background // 테마의 배경색으로 설정
                 ) {
                     Greeting("Test")
                 }
@@ -36,101 +38,96 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 인사말과 여러 버튼을 포함하는 컴포넌트 정의
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
+    val context = LocalContext.current // 현재의 Context를 가져옴
 
     Column(
         modifier = modifier
     ) {
+        // 각 버튼마다 서로 다른 기능을 수행하는 Intent를 실행
+        // 유튜브 링크를 열기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("https://www.youtube.com/results?search_query=android+developers")
+                val uri = Uri.parse("https://www.youtube.com/results?search_query=android+developers")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(context, intent, null)
             }
         ) {
             Text("유튜브")
         }
+        // 인스타그램 링크를 열기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("https://www.instagram.com/newjeans_official/")
+                val uri = Uri.parse("https://www.instagram.com/newjeans_official/")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(context, intent, null)
             }
         ) {
             Text("인스타그램")
         }
+        // 깃허브 링크를 열기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("https://github.com/joon6093")
+                val uri = Uri.parse("https://github.com/joon6093")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(context, intent, null)
             }
         ) {
             Text("깃허브")
         }
+        // 특정 좌표로 지도를 열기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("geo:36.145014,128.393047?z=17")
+                val uri = Uri.parse("geo:36.145014,128.393047?z=17")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(context, intent, null)
             }
         ) {
             Text("좌표")
         }
+        // 주소나 상호명으로 지도를 열기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("geo:0,0?q=금오공과대학교")
+                val uri = Uri.parse("geo:0,0?q=금오공과대학교")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(context, intent, null)
             }
         ) {
             Text("주소 또는 상호")
         }
+        // 전화를 걸기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("tel:0540001234")
+                val uri = Uri.parse("tel:0540001234")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(context, intent, null)
             }
         ) {
             Text("전화")
         }
+        // 문자를 보내기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
-                val uri =
-                    Uri.parse("sms:01000001234")
+                val uri = Uri.parse("sms:01000001234")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
-                intent.putExtra("sms_body", "전화 부탁드립니다")
+                intent.putExtra("sms_body", "전화 부탁드립니다") // 문자 내용 설정
                 startActivity(context, intent, null)
             }
         ) {
             Text("문자")
         }
+        // SecondActivity로 이동하기 위한 버튼
         Button(
-            modifier = modifier
-                .fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             onClick = {
                 val intent = Intent(context, SecondActivity::class.java)
                 startActivity(context, intent, null)
@@ -141,6 +138,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
+// 프리뷰 함수: UI 디자인을 미리보기 환경에서 확인할 수 있게 해줌
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
--- Android-study/S23W06Intent/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w06intent/SecondActivity.kt
+++ Android-study/S23W06Intent/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w06intent/SecondActivity.kt
@@ -12,16 +12,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.ce.s20190633.s23w06intent.ui.theme.S23W06IntentTheme
 
+// 두 번째 액티비티 클래스 정의
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // 앱의 테마를 설정하여 UI 컴포넌트들을 표시
             S23W06IntentTheme {
-                // A surface container using the 'background' color from the theme
+                // 백그라운드 색상을 테마의 'background' 색상으로 설정한 컨테이너
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // "Android"라는 텍스트로 인사말 컴포넌트를 표시
                     Greeting2("Android")
                 }
             }
@@ -29,14 +32,17 @@ class SecondActivity : ComponentActivity() {
     }
 }
 
+// 인사말 컴포넌트 정의
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    // "Hello [name]!" 형태의 텍스트를 표시
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
 }
 
+// 프리뷰 함수: UI 디자인을 미리보기 환경에서 확인할 수 있게 해줌
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
