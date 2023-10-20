b0f33abfb934b35d0b7238c4e5c5a7f42d9be6ad
Jeyong <joon6093@naver.com>
2023-10-21 02:39:34 +0900

    [ADD] 코드 주석 추가

--- Android-study/S23W03Lotto/app/src/main/java/kr/ac/kumoh/ce/s20190633/S23W03Lotto/LottoViewModel.kt
+++ Android-study/S23W03Lotto/app/src/main/java/kr/ac/kumoh/ce/s20190633/S23W03Lotto/LottoViewModel.kt
@@ -1,30 +1,41 @@
+// 패키지 정의
 package kr.ac.kumoh.ce.s20190633.S23W03Lotto
 
+// 필요한 Android 및 Kotlin 라이브러리와 클래스들을 임포트
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
-class LottoViewModel : ViewModel( ) {
+// ViewModel 클래스를 상속 받아 정의된 LottoViewModel 클래스
+class LottoViewModel : ViewModel() {
+
+    // MutableLiveData는 변경 가능한 데이터를 저장하며, UI 컴포넌트와 데이터의 동기화를 돕습니다.
     private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
+
+    // numbers는 _numbers의 변경 불가능한 버전으로, UI에서 이를 관찰하게 됩니다.
     val numbers: LiveData<IntArray>
         get() = _numbers
+
+    // 로또 번호를 생성하는 메서드
     fun generate() {
         var num = 0
-        val newNumbers = IntArray(6) { 0 }
-        //newNumbers.forEach { Log.i("newNumbers!!!", "$it") }
+        val newNumbers = IntArray(6) { 0 } // 새로운 로또 번호를 저장할 배열 초기화
 
+        // 각 로또 번호에 대해
         for (i in newNumbers.indices) {
             // 중복 검사
             do {
+                // 1부터 45 사이의 임의의 번호 생성
                 num = Random.nextInt(1, 46)
-            } while (newNumbers.contains(num))
+            } while (newNumbers.contains(num)) // 이미 배열에 존재하는 번호인 경우 다시 생성
             newNumbers[i] = num
         }
 
-        // 정렬
+        // 로또 번호 배열을 정렬
         newNumbers.sort()
 
+        // _numbers MutableLiveData에 새로운 로또 번호 배열을 할당
         _numbers.value = newNumbers
     }
 }
--- Android-study/S23W03Lotto/app/src/main/java/kr/ac/kumoh/ce/s20190633/S23W03Lotto/MainActivity.kt
+++ Android-study/S23W03Lotto/app/src/main/java/kr/ac/kumoh/ce/s20190633/S23W03Lotto/MainActivity.kt
@@ -1,5 +1,7 @@
+// 패키지 정의
 package kr.ac.kumoh.ce.s20190633.S23W03Lotto
 
+// 필요한 Android 라이브러리와 클래스들을 임포트
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,40 +10,49 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20190633.S23W03Lotto.databinding.ActivityMainBinding
 
+// MainActivity 정의. AppCompatActivity를 상속 받음
 class MainActivity : AppCompatActivity() {
+    // 뷰 바인딩, 뷰모델 및 텍스트뷰 배열에 대한 멤버 변수 선언
     private lateinit var main: ActivityMainBinding
     private lateinit var model: LottoViewModel
     private lateinit var txtNum: Array<TextView?>
 
+    // 액티비티가 생성될 때 호출되는 콜백 메서드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("Lifecycle!!!", "onCreate")
-
-        //setContentView(R.layout.activity_main)
+        Log.i("Lifecycle!!!", "onCreate") // 로그 메시지로 생명주기 확인
+        //setContentView(R.layout.activity_main)  ->  setContentView(R.layout.activity_main)은 액티비티에 activity_main.xml 레이아웃 파일의 내용을 화면에 표시하도록 설정하는 것입니다.
+        // 뷰 바인딩을 사용하여 레이아웃 파일을 설정
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        // 텍스트뷰 배열을 뷰 바인딩으로 초기화
         txtNum = arrayOf(main.num1, main.num2, main.num3, main.num4, main.num5, main.num6)
 
+        // 뷰모델 인스턴스 초기화
         model = ViewModelProvider(this)[LottoViewModel::class.java]
-        setNumbersText()
+        setNumbersText() // 초기 로또 번호 텍스트 설정
 
+        // 로또 번호가 변경될 때마다 UI 업데이트
         model.numbers.observe(this, Observer {
             Log.i("Observer!!!", "numbers changed")
             setNumbersText()
         })
 
+        // 버튼 클릭시 로또 번호 생성
         main.btnGenerate.setOnClickListener {
             model.generate()
-            //setNumbersText()
         }
     }
 
+    // 로또 번호 텍스트뷰를 뷰모델의 로또 번호 값으로 설정하는 메서드
     private fun setNumbersText() {
         txtNum.forEachIndexed { index, textView ->
             textView?.text = model.numbers.value!![index].toString()
         }
     }
+
+    // 액티비티의 생명주기 메서드들. 각 메서드는 로그 메시지를 사용하여 생명주기 상태를 확인
     override fun onStart() {
         super.onStart()
         Log.i("Lifecycle!!!", "onStart")
