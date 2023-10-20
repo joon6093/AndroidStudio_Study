cb72a9afdcf178648fc18858517b07f251dce9f7
Jeyong <joon6093@naver.com>
2023-10-21 03:49:06 +0900

    [ADD] 코드 주석 추가

--- Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/CardDealerViewModel.kt
+++ Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/CardDealerViewModel.kt
@@ -1,29 +1,43 @@
+// 패키지 정의
 package kr.ac.kumoh.ce.s20190633.s23w04carddealer
 
+// 필요한 Android 라이브러리와 클래스들을 임포트
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
+// 카드 딜러 앱의 ViewModel 정의
 class CardDealerViewModel : ViewModel() {
+    // _cards는 내부에서만 변경 가능한 LiveData로서, 초기 값으로 -1을 가진 5개의 정수 배열을 갖는다.
     private var _cards = MutableLiveData<IntArray>(IntArray(5) { -1 })
+
+    // cards는 외부에서 읽기 전용으로 접근 가능한 LiveData이다.
     val cards: LiveData<IntArray>
         get() = _cards
+
+    // 카드를 셔플하는 함수
     fun shuffle() {
         var num = 0
+        // 셔플된 새 카드를 저장할 배열을 생성. 초기 값으로 -1을 가진다.
         val newCards = IntArray(5) { -1 }
 
+        // 각 카드 위치에 대해
         for (i in newCards.indices) {
-            // 중복 검사
+            // 중복되지 않는 카드 번호를 생성하기 위한 반복문
             do {
+                // 0부터 51 사이의 무작위 수(총 52개의 카드)를 생성
                 num = Random.nextInt(52)
-            } while (newCards.contains(num))
+            } while (newCards.contains(num)) // 이미 선택된 카드 번호인 경우 다시 무작위 수를 생성
+
+            // 셔플된 카드 번호를 배열에 저장
             newCards[i] = num
         }
 
-        // 정렬
+        // 셔플된 카드 번호를 오름차순으로 정렬
         newCards.sort()
 
+        // 셔플된 카드 번호를 _cards에 저장하여 LiveData를 업데이트
         _cards.value = newCards
     }
 }
--- Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/MainActivity.kt
+++ Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/MainActivity.kt
@@ -1,45 +1,66 @@
+// 패키지 정의
 package kr.ac.kumoh.ce.s20190633.s23w04carddealer
 
+// 필요한 Android 라이브러리와 클래스들을 임포트
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20190633.s23w04carddealer.databinding.ActivityMainBinding
 
-
+// MainActivity 정의. AppCompatActivity를 상속 받음
 class MainActivity : AppCompatActivity() {
+    // 뷰 바인딩 변수와 ViewModel 변수 선언
     private lateinit var main: ActivityMainBinding
     private lateinit var model: CardDealerViewModel
+
+    // 액티비티가 생성될 때 호출되는 콜백 메서드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 뷰 바인딩을 사용하여 레이아웃 파일을 설정
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        // ViewModel 인스턴스를 가져옴
         model = ViewModelProvider(this)[CardDealerViewModel::class.java]
+
+        // ViewModel의 cards를 관찰하고 변화가 있을 때 UI 업데이트
         model.cards.observe(this, Observer {
             val res = IntArray(5)
             for (i in it.indices) {
+                Log.i("it:", it[i].toString())
+                Log.i("getCardName:", getCardName(it[i]).toString())
                 res[i] = resources.getIdentifier(
                     getCardName(it[i]),
                     "drawable",
                     packageName
                 )
+                Log.i("res:", res[i].toString())
             }
-
+            // 첫 번째 카드 이미지를 설정
             main.card1.setImageResource(res[0])
-//            main.card2.setImageResource(res[1])
-//            main.card3.setImageResource(res[2])
-//            main.card4.setImageResource(res[3])
-//            main.card5.setImageResource(res[4])
+            // 아래는 주석 처리된 다른 카드들의 이미지 설정 코드
+            // main.card2.setImageResource(res[1])
+            // main.card3.setImageResource(res[2])
+            // main.card4.setImageResource(res[3])
+            // main.card5.setImageResource(res[4])
         })
 
+        // '셔플' 버튼을 클릭했을 때의 동작 설정
         main.btnShuffle.setOnClickListener {
             model.shuffle()
         }
     }
 
+    // 카드 번호를 받아 해당하는 카드 이름(문자열)을 반환하는 함수
     private fun getCardName(c: Int) : String {
-        // val에서 var로 변경
+        // 주어진 값이 -1이면 조커 카드를 의미한다고 가정하고 바로 "c_red_joker"를 반환
+        if (c == -1) {
+            return "c_red_joker"
+        }
+        // 카드 모양에 따른 이름을 결정
         var shape = when (c / 13) {
             0 -> "spades"
             1 -> "diamonds"
@@ -48,6 +69,7 @@ class MainActivity : AppCompatActivity() {
             else -> "error"
         }
 
+        // 카드 숫자에 따른 이름을 결정
         val number = when (c % 13) {
             0 -> "ace"
             in 1..9 -> (c % 13 + 1).toString()
@@ -66,12 +88,7 @@ class MainActivity : AppCompatActivity() {
             else -> "error"
         }
 
-        // 이 방법이 더 나을 듯
-//        if (c % 13 in 10..12)
-//            return "c_${number}_of_${shape}2"
-//        else
-//            return "c_${number}_of_${shape}"
-
+        // 카드 이름을 반환
         return "c_${number}_of_${shape}"
     }
 }
