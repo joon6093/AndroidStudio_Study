5b1028f6d797a8cea9f7382beea706b381bbee7e
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-10-23 22:30:40 +0900

    [ADD] 코드 주석 추가

--- Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/ImageActivity.kt
+++ Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/ImageActivity.kt
@@ -7,44 +7,74 @@ import android.view.View
 import android.view.View.OnClickListener
 import kr.ac.kumoh.ce.s20190633.s23w07intentdata.databinding.ActivityImageBinding
 
+// 이미지 평가를 위한 액티비티 클래스 정의
 class ImageActivity : AppCompatActivity(), OnClickListener {
+
+    // 동반 객체(Companion Object): 정적 변수나 함수를 정의할 때 사용
     companion object {
         const val IMAGE_NAME = "image name"
         const val IMAGE_RESULT = "image result"
 
+        // 이미지 평가 상수 정의
         const val LIKE = 100
         const val DISLIKE = 101
         const val NONE = 0
     }
+
+    // 액티비티에서 사용할 뷰 바인딩 객체 선언
     private lateinit var main: ActivityImageBinding
+
+    // 선택된 메카닉의 이름을 저장할 변수 선언
     private lateinit var mechanic: String
 
+    // 액티비티가 생성될 때 호출되는 함수
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 뷰 바인딩 초기화
         main = ActivityImageBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        // MainActivity로부터 전달받은 메카닉 이름을 가져옴
         mechanic = intent.getStringExtra(MainActivity.KEY_NAME) ?: "none"
+
+        // 메카닉 이름에 따른 이미지 리소스 결정
         val res = when (mechanic) {
             MainActivity.GUNDAM -> R.drawable.gundam
             MainActivity.ZAKU -> R.drawable.zaku
             else -> R.drawable.ic_launcher_foreground
         }
+
+        // 이미지 뷰에 리소스 설정
         main.image.setImageResource(res)
 
+        // 좋아요와 싫어요 버튼에 클릭 리스너 설정
         main.btnLike.setOnClickListener(this)
         main.btnDislike.setOnClickListener(this)
     }
 
+    // 버튼 클릭 시 호출되는 함수
     override fun onClick(p0: View?) {
-        val intent = Intent()
+        // 결과를 반환하기 위한 Intent 객체 생성
+        val intent = Intent()  //  val intent = Intent(this, MainActivity::class.java)
+        // 클릭된 버튼에 따라 평가 값 설정
         val value = when (p0?.id) {
-            main.btnLike.id -> LIKE
-            main.btnDislike.id -> DISLIKE
-            else -> NONE
+            main.btnLike.id -> {
+                LIKE
+            }
+            main.btnDislike.id -> {
+                DISLIKE
+            }
+            else -> {
+                NONE
+            }
         }
+
+        // 선택된 메카닉 이름과 평가 값을 Intent에 추가
         intent.putExtra(IMAGE_NAME, mechanic)
         intent.putExtra(IMAGE_RESULT, value)
+
+        // 결과를 MainActivity에 전달하고 액티비티 종료
         setResult(RESULT_OK, intent)
         finish()
     }
--- Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/MainActivity.kt
+++ Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/MainActivity.kt
@@ -10,44 +10,63 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.ce.s20190633.s23w07intentdata.databinding.ActivityMainBinding
 
+// 메인 액티비티 클래스 정의. 이 액티비티에서는 두 버튼(건담, 자쿠)을 제공하며, 각 버튼을 클릭하면
+// 해당하는 이미지를 보여주는 ImageActivity로 이동한다.
 class MainActivity : AppCompatActivity(), OnClickListener {
+
+    // 동반 객체. 액티비티 간 데이터 전송에 사용되는 키와 값들을 상수로 정의한다.
     companion object {
         const val KEY_NAME = "mechanic"
         const val GUNDAM = "gundam"
         const val ZAKU = "zaku"
     }
 
+    // 뷰 바인딩 객체. XML 레이아웃의 뷰와 연결된다.
     private lateinit var main: ActivityMainBinding
+
+    // ImageActivity로부터 결과를 받아오기 위한 콜백 정의.
     private val startForResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()) {
+        // 결과가 OK가 아닌 경우에는 아무 처리도 하지 않고 반환한다.
         if (it.resultCode != Activity.RESULT_OK)
             return@registerForActivityResult
 
+        // ImageActivity에서 반환된 사용자의 평가(좋아요/싫어요)를 가져온다.
         val result = it.data?.getIntExtra(
             ImageActivity.IMAGE_RESULT,
-            ImageActivity.NONE) // 없을 때 반환값
+            ImageActivity.NONE) // 결과가 없을 때 기본값
+
+        // 사용자 평가를 문자열로 변환한다.
         val str = when (result) {
             ImageActivity.LIKE -> "좋아요"
             ImageActivity.DISLIKE -> "싫어요"
             else -> ""
         }
+        // 선택된 메카닉 이름에 따라 해당하는 버튼의 텍스트를 업데이트한다.
         when (it.data?.getStringExtra(ImageActivity.IMAGE_NAME)) {
             GUNDAM -> main.btnGundam.text = "건담 ($str)"
             ZAKU -> main.btnZaku.text = "자쿠 ($str)"
         }
     }
 
+    // 액티비티가 처음 생성될 때 호출되는 메서드.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 뷰 바인딩 객체를 초기화하고, 뷰를 액티비티에 연결한다.
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        // 건담 및 자쿠 버튼에 클릭 리스너를 설정한다. 클릭 시 ImageActivity로 이동한다.
         main.btnGundam.setOnClickListener(this)
         main.btnZaku.setOnClickListener(this)
     }
 
+    // 버튼 클릭 시 호출되는 메서드.
     override fun onClick(p0: View?) {
+        // ImageActivity로 이동하기 위한 Intent 객체를 생성한다.
         val intent = Intent(this, ImageActivity::class.java)
+
+        // 클릭된 버튼에 따라 메카닉 이름을 설정하고, 해당하는 메카닉의 이름을 토스트 메시지로 보여준다.
         val value = when (p0?.id) {
             main.btnGundam.id -> {
                 Toast.makeText(this, "건담 이미지", Toast.LENGTH_SHORT).show()
@@ -59,8 +78,10 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             }
             else -> return
         }
+        // 선택된 메카닉 이름을 Intent에 추가한다.
         intent.putExtra(KEY_NAME, value)
-        //startActivity(intent)
+
+        // Intent를 사용하여 ImageActivity를 시작하고, 그 결과를 기다린다.
         startForResult.launch(intent)
     }
 }
