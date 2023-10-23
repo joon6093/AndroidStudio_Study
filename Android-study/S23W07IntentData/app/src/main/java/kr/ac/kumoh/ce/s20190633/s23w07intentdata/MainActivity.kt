062250e6e87453914a6fa5d63e812919a8cb046a
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-10-23 14:41:43 +0900

    [ADD] OnClickListener 사용하기

--- Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/MainActivity.kt
+++ Android-study/S23W07IntentData/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w07intentdata/MainActivity.kt
@@ -3,10 +3,12 @@ package kr.ac.kumoh.ce.s20190633.s23w07intentdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import kr.ac.kumoh.ce.s20190633.s23w07intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     companion object {
         const val KEY_NAME = "mechanic"
         const val GUNDAM = "gundam"
@@ -19,20 +21,24 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.btnGundam.setOnClickListener {
-            Toast.makeText(this, "건담 이미지", Toast.LENGTH_SHORT).show()
+        main.btnGundam.setOnClickListener(this)
+        main.btnZaku.setOnClickListener(this)
+    }
 
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, GUNDAM)
-            startActivity(intent)
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, ImageActivity::class.java)
+        val value = when (p0?.id) {
+            main.btnGundam.id -> {
+                Toast.makeText(this, "건담 이미지", Toast.LENGTH_SHORT).show()
+                GUNDAM
+            }
+            main.btnZaku.id -> {
+                Toast.makeText(this, "자쿠 이미지", Toast.LENGTH_SHORT).show()
+                ZAKU
+            }
+            else -> return
         }
-
-        main.btnZaku.setOnClickListener {
-            Toast.makeText(this, "자쿠 이미지", Toast.LENGTH_SHORT).show()
-
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, ZAKU)
-            startActivity(intent)
-        }
+        intent.putExtra(KEY_NAME, value)
+        startActivity(intent)
     }
 }
