874508edd61cffeaec020e2065be2b28525423ee
Jeyong <joon6093@naver.com>
2023-11-07 01:06:06 +0900

    [ADD] 코드 주석 추가

--- Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/CardDealerViewModel.kt
+++ Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/CardDealerViewModel.kt
@@ -1,7 +1,5 @@
-// 패키지 정의
 package kr.ac.kumoh.ce.s20190633.s23w04carddealer
 
-// 필요한 Android 라이브러리와 클래스들을 임포트
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,11 +9,11 @@ import kotlinx.coroutines.*
 
 // 카드 딜러 앱의 ViewModel 정의
 class CardDealerViewModel : ViewModel() {
-    // _cards는 내부에서만 변경 가능한 LiveData로서, 초기 값으로 -1을 가진 5개의 정수 배열을 갖는다.
-    private var _cards = MutableLiveData<IntArray>(intArrayOf(-1, -1, -1, -1, -1 ))
-    // cards는 외부에서 읽기 전용으로 접근 가능한 LiveData이다.
+    // 초기로 -1을 가짐 -> 조커 카드
+    private var _cards = MutableLiveData<IntArray>(intArrayOf(-1, -1, -1, -1, -1))
     val cards: LiveData<IntArray>
         get() = _cards
+
     // 포커 족보를 저장할 LiveData 추가
     private var _handType = MutableLiveData<String>("")
     val handType: LiveData<String>
@@ -24,7 +22,6 @@ class CardDealerViewModel : ViewModel() {
     // 카드를 셔플하는 함수
     fun shuffle() {
         var num = 0
-        // 셔플된 새 카드를 저장할 배열을 생성. 초기 값으로 -1을 가진다.
         val newCards = IntArray(5) { -1 }
 
         // 각 카드 위치에 대해
@@ -44,21 +41,26 @@ class CardDealerViewModel : ViewModel() {
 
         // 셔플된 카드 번호를 _cards에 저장하여 LiveData를 업데이트
         _cards.value = newCards
+        // 족보 결정 및 업데이트
         _handType.value = determineHand(newCards)
     }
+
+    // 카드 배열을 받아 포커 족보를 결정하는 함수
     private fun determineHand(cards: IntArray): String {
         val suits = Array(4) { 0 }  // 각 카드 무늬별 개수
         val values = Array(13) { 0 }  // 각 카드 숫자별 개수
 
+        // 카드들을 순회하며 무늬와 숫자별 개수를 계산
         for (card in cards) {
             suits[card / 13]++
             values[card % 13]++
         }
 
+        // 중복되지 않는 숫자의 개수와 가장 많은 숫자 카드의 개수를 확인
         val distinctValues = values.filter { it > 0 }.size
         val maxOfValues = values.maxOrNull() ?: 0
 
-        // 족보 판별
+        // 족보 판별 로직
         return when {
             hasRoyalStraight(values) && hasFlush(suits) -> "로얄 플러시"
             hasStraight(values) && hasFlush(suits) -> "스트레이트 플러시"
@@ -73,10 +75,12 @@ class CardDealerViewModel : ViewModel() {
         }
     }
 
+    // 로얄 스트레이트 여부 확인 함수
     private fun hasRoyalStraight(values: Array<Int>): Boolean {
         return values[0] > 0 && values[1] > 0 && values[10] > 0 && values[11] > 0 && values[12] > 0
     }
 
+    // 스트레이트 여부 확인 함수
     private fun hasStraight(values: Array<Int>): Boolean {
         // 일반 스트레이트 확인
         for (i in 0 until 9) {
@@ -93,20 +97,25 @@ class CardDealerViewModel : ViewModel() {
         return false
     }
 
+    // 플러시 여부 확인 함수
     private fun hasFlush(suits: Array<Int>): Boolean {
         return suits.any { it == 5 }
     }
 
+    // 시뮬레이션 결과를 저장할 LiveData
     private var _simulationResult = MutableLiveData<String>()
     val simulationResult: LiveData<String>
         get() = _simulationResult
 
+    // 시뮬레이션을 위한 코루틴 Job 변수
     private var simulationJob: Job? = null
 
+    // 주어진 횟수만큼 족보 시뮬레이션을 실행하는 함수
     fun simulate(times: Int) {
         simulationJob = viewModelScope.launch {
             val handCounts = mutableMapOf<String, Int>()
             var results = ""
+            // 지정된 횟수만큼 족보를 생성하고 통계를 계산
             for (i in 0 until times) {
                 shuffle()
                 delay(300) // 시각적 효과를 위한 지연
@@ -114,6 +123,7 @@ class CardDealerViewModel : ViewModel() {
                 val hand = _handType.value ?: "Error"
                 handCounts[hand] = handCounts.getOrDefault(hand, 0) + 1
 
+                // 마지막 반복에서만 확률 계산
                 if (i == times - 1) {
                     results = handCounts.entries.joinToString("\n") { (hand, count) ->
                         val probability = count.toDouble() / times * 100
@@ -129,9 +139,13 @@ class CardDealerViewModel : ViewModel() {
             }
         }
     }
+
+    // 시뮬레이션 취소 함수
     fun cancelSimulation() {
         simulationJob?.cancel()
     }
+
+    // 시뮬레이션이 진행 중인지 확인하는 함수
     fun isSimulationInProgress(): Boolean {
         return simulationJob?.isActive == true
     }
--- Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/MainActivity.kt
+++ Android-study/S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w04carddealer/MainActivity.kt
@@ -1,11 +1,8 @@
-// 패키지 정의
 package kr.ac.kumoh.ce.s20190633.s23w04carddealer
 
-// 필요한 Android 라이브러리와 클래스들을 임포트
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
-import android.util.Log
 import android.view.Gravity
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
@@ -13,52 +10,46 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20190633.s23w04carddealer.databinding.ActivityMainBinding
 
-// MainActivity 정의. AppCompatActivity를 상속 받음
 class MainActivity : AppCompatActivity() {
-    // 뷰 바인딩 변수와 ViewModel 변수 선언
     private lateinit var main: ActivityMainBinding
     private lateinit var model: CardDealerViewModel
     private var simulationDialog: AlertDialog? = null
-    // 액티비티가 생성될 때 호출되는 콜백 메서드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 뷰 바인딩을 사용하여 레이아웃 파일을 설정
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        // ViewModel 인스턴스를 가져옴
         model = ViewModelProvider(this)[CardDealerViewModel::class.java]
 
-        // ViewModel의 cards를 관찰하고 변화가 있을 때 UI 업데이트
         model.cards.observe(this, Observer {
             val res = IntArray(5)
             for (i in it.indices) {
-//                Log.i("it:", it[i].toString())
-//                Log.i("getCardName:", getCardName(it[i]).toString())
+                // 카드 리소스 식별자를 가져와 배열에 저장
                 res[i] = resources.getIdentifier(
                     getCardName(it[i]),
                     "drawable",
                     packageName
                 )
-//                Log.i("res:", res[i].toString())
             }
-            // 카드 이미지를 설정
             main.card1.setImageResource(res[0])
             main.card2.setImageResource(res[1])
             main.card3.setImageResource(res[2])
             main.card4.setImageResource(res[3])
             main.card5.setImageResource(res[4])
         })
+        // 족보 결과 텍스트 뷰 업데이트
         model.handType.observe(this, Observer {
             main.tvHandType.text = it
         })
 
-        // '셔플' 버튼을 클릭했을 때의 동작 설정
+        // '한 판만' 버튼 클릭 이벤트 설정
         main.btnShuffle.setOnClickListener {
             model.shuffle()
         }
+        // 시뮬레이션 결과 LiveData 관찰 및 업데이트
         model.simulationResult.observe(this, Observer { result ->
+            // 시뮬레이션 다이얼로그 설정
             if (simulationDialog == null) {
                 simulationDialog = AlertDialog.Builder(this)
                     .setTitle("시뮬레이션 결과")
@@ -72,7 +63,7 @@ class MainActivity : AppCompatActivity() {
                 simulationDialog?.setMessage(result)
             }
 
-            // 시뮬레이션이 진행 중인지 여부에 따라 버튼 텍스트 설정
+            // 시뮬레이션 진행 중 여부에 따라 버튼 텍스트 설정
             val isSimulationInProgress = model.isSimulationInProgress()
             simulationDialog?.getButton(AlertDialog.BUTTON_NEGATIVE)?.text =
                 if (isSimulationInProgress) "취소" else "확인"
@@ -87,11 +78,12 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        // 시뮬레이션 버튼 클릭 이벤트 설정
         main.btnSimulation.setOnClickListener {
-            // 다이얼로그 생성 및 입력 받기
+            // 시뮬레이션 횟수 입력 다이얼로그 생성 및 표시
             val dialog = AlertDialog.Builder(this)
             dialog.setTitle("시뮬레이션")
-            dialog.setMessage("시뮬레이션 횟수를 입력하세요")
+            dialog.setMessage("시뮬레이션 횟수를 입력하세요:")
 
             val input = EditText(this)
             input.inputType = InputType.TYPE_CLASS_NUMBER
@@ -112,11 +104,11 @@ class MainActivity : AppCompatActivity() {
 
     // 카드 번호를 받아 해당하는 카드 이름(문자열)을 반환하는 함수
     private fun getCardName(c: Int) : String {
-        // 주어진 값이 -1이면 조커 카드를 의미한다고 가정하고 바로 "c_red_joker"를 반환
+        // -1일 경우 조커 카드로 가정하고 "c_red_joker" 반환
         if (c == -1) {
             return "c_red_joker"
         }
-        // 카드 모양에 따른 이름을 결정
+        // 카드 모양에 따른 이름 결정
         var shape = when (c / 13) {
             0 -> "spades"
             1 -> "diamonds"
@@ -125,7 +117,7 @@ class MainActivity : AppCompatActivity() {
             else -> "error"
         }
 
-        // 카드 숫자에 따른 이름을 결정
+        // 카드 숫자에 따른 이름 결정
         val number = when (c % 13) {
             0 -> "ace"
             in 1..9 -> (c % 13 + 1).toString()
@@ -144,7 +136,7 @@ class MainActivity : AppCompatActivity() {
             else -> "error"
         }
 
-        // 카드 이름을 반환
+        // 최종 카드 이름 반환
         return "c_${number}_of_${shape}"
     }
 
