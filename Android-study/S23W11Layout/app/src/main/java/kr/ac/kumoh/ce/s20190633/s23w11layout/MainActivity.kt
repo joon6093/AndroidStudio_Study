e892c9e3b10f28441eeb6247089dcccdd8ce9499
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-13 14:25:34 +0900

    [INIT] 초기 커밋

---
+++ Android-study/S23W11Layout/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w11layout/MainActivity.kt
@@ -0,0 +1,50 @@
+package kr.ac.kumoh.ce.s20190633.s23w11layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.ce.s20190633.s23w11layout.ui.theme.S23W11LayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Myapp()
+        }
+    }
+}
+@Composable
+fun Myapp() {
+    S23W11LayoutTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S23W11LayoutTheme {
+        Greeting("Android")
+    }
+}
