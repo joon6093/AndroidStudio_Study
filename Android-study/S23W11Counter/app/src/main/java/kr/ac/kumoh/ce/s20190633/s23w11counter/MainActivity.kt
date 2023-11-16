919c6ed5448fc68a7b6af8dec00c666f43d8d722
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-16 13:37:45 +0900

    [UPDATE] mutableStateOf 변수를 이용해서 상태 바꾸기

--- Android-study/S23W11Counter/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w11counter/MainActivity.kt
+++ Android-study/S23W11Counter/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w11counter/MainActivity.kt
@@ -3,44 +3,71 @@ package kr.ac.kumoh.ce.s20190633.s23w11counter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20190633.s23w11counter.ui.theme.S23W11CounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            S23W11CounterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            myApp {
+                Clicker()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Clicker() {
+    //var txtString = "눌러주세요";
+    var txtString by remember { mutableStateOf("눌러주세요") }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = txtString,
+            fontSize = 70.sp,
+        )
+        Button(modifier = Modifier
+            .fillMaxWidth(),
+            onClick = {
+                txtString = "눌렸습니다"
+            }) {
+            // Text(text = "눌러봐")
+            Text("눌러봐")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun myApp(content : @Composable () -> Unit){
     S23W11CounterTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
     }
 }
