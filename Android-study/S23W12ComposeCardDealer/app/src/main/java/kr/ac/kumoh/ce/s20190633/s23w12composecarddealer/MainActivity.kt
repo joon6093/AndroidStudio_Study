0c45cfa06cede68dc323379664ac7e80e05ec3e8
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-20 14:25:45 +0900

    [INIT] 초기커밋

---
+++ Android-study/S23W12ComposeCardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w12composecarddealer/MainActivity.kt
@@ -0,0 +1,43 @@
+package kr.ac.kumoh.ce.s20190633.s23w12composecarddealer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.ce.s20190633.s23w12composecarddealer.ui.theme.S23W12ComposeCardDealerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            S23W12ComposeCardDealerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S23W12ComposeCardDealerTheme {
+        Greeting("Android")
+    }
+}
