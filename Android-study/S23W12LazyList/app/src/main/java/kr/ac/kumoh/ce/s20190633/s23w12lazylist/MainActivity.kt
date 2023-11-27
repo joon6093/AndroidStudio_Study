7647f37f105d5af2368cf7b0b1d6783cd7b24c02
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-27 18:45:06 +0900

    [ADD] LazyColumn 사용해보기

--- Android-study/S23W12LazyList/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w12lazylist/MainActivity.kt
+++ Android-study/S23W12LazyList/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w12lazylist/MainActivity.kt
@@ -3,13 +3,22 @@ package kr.ac.kumoh.ce.s20190633.s23w12lazylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20190633.s23w12lazylist.ui.theme.S23W12LazyListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyList()
                 }
             }
         }
@@ -30,17 +39,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SongItem(index: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc)) // 순서 중요함 -> 패딩 먼저하냐 나중에 하냐
+            .padding(16.dp)
+    ) {
+        TextTitle("노래 $index")
+        TextSinger("이 노래를 부른 가수는 $index 입니다")
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S23W12LazyListTheme {
-        Greeting("Android")
+fun TextTitle(title: String) {
+    Text(title, fontSize = 30.sp)
+}
+
+@Composable
+fun TextSinger(singer: String) {
+    Text(singer, fontSize = 20.sp)
+}
+
+@Composable
+fun MyList() {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp), // 아이템들 사이를 8dp 간격으로
+        contentPadding = PaddingValues(horizontal = 8.dp) // 아이템 안에서 가로축만 8dp 간격 , 현재 세로는 설정 x
+    ) {
+        items(30) {
+            SongItem(it)
+        }
     }
 }
