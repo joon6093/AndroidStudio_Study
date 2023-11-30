a3aa3e5c96341e60e3a91c1e24ff969aef65d5fe
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-30 14:45:58 +0900

    [UPDATE] Card 사용해보기

--- Android-study/S23W13SongList/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w13songlist/MainActivity.kt
+++ Android-study/S23W13SongList/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w13songlist/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -19,13 +21,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -63,40 +71,53 @@ fun MainScreen(viewModel: SongViewModel) {
 @Composable
 fun SongList(list: List<Song>) {
     LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(horizontal = 8.dp)
     ) {
-        items(list) { song ->
-            SongItem(song)
+        itemsIndexed(list) { index, song ->
+            SongItem(index, song)
         }
     }
 }
 
 @Composable
-fun SongItem(song: Song) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Min)
-            .background(Color(255, 210, 210))
-            .padding(8.dp)
+fun SongItem(index: Int, song: Song) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier.clickable { expanded = !expanded },
+        elevation = CardDefaults.cardElevation(8.dp)
     ) {
-        AsyncImage(
-            model = "https://picsum.photos/300/300?random=${song.id}",
-            contentDescription = "노래 앨범 이미지",
-            contentScale = ContentScale.Crop,
+        Row(
             modifier = Modifier
-                .size(100.dp)
-                //.clip(CircleShape),
-                .clip(RoundedCornerShape(percent = 10)),
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                //.background(Color(255, 210, 210))
+                .padding(8.dp)
         ) {
-            TextTitle(song.title)
-            TextSinger(song.singer)
+            AsyncImage(
+                model = "https://picsum.photos/300/300?random=$index",
+                contentDescription = "노래 앨범 이미지",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    //.clip(CircleShape),
+                    .clip(RoundedCornerShape(percent = 10)),
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround
+                //verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                TextTitle(song.title)
+                TextSinger(song.singer)
+            }
+
+        }
+        AnimatedVisibility(visible = expanded) {
+            song.lyrics?.let { Text(it) }
         }
     }
 }
