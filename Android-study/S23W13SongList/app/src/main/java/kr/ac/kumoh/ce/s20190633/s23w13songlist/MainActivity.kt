eaad801dea671e6845b1bdedfda6e0411879dda5
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-30 13:53:51 +0900

    {UPDATE] AsyncImage 사용해보기

--- Android-study/S23W13SongList/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w13songlist/MainActivity.kt
+++ Android-study/S23W13SongList/app/src/main/java/kr/ac/kumoh/ce/s20190633/s23w13songlist/MainActivity.kt
@@ -7,12 +7,19 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,9 +27,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import kr.ac.kumoh.ce.s20190633.s23w13songlist.ui.theme.S23W13SongListTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,14 +74,30 @@ fun SongList(list: List<Song>) {
 
 @Composable
 fun SongItem(song: Song) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color(0xffffffcc))
-            .padding(16.dp)
+            .height(IntrinsicSize.Min)
+            .background(Color(255, 210, 210))
+            .padding(8.dp)
     ) {
-        TextTitle(song.title)
-        TextSinger("이 노래를 부른 가수는 ${song.singer} 입니다")
+        AsyncImage(
+            model = "https://picsum.photos/300/300?random=${song.id}",
+            contentDescription = "노래 앨범 이미지",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                //.clip(CircleShape),
+                .clip(RoundedCornerShape(percent = 10)),
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextTitle(song.title)
+            TextSinger(song.singer)
+        }
     }
 }
 
